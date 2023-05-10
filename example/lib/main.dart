25ce0395b72863829aec2d9fc7ce748c84f17fd0
vaibhavEleannt <112240240+vaibhavEleannt@users.noreply.github.com>
2023-05-11 00:51:27 +0530

    fixes

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:custom_loading/custom_loading.dart';
 import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -39,17 +40,9 @@ class _MyHomePageState extends State<MyHomePage> {
     return CustomLoadingScaffold(
       isLoading: isLoading,
       blurIntensity: 2.0,
-      loaderWidget: Container(
-        height: 100,
-        padding: const EdgeInsets.all(15),
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10), color: Colors.grey),
-        child: const Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CircularProgressIndicator(),
-          ],
-        ),
+      loaderWidget: LoadingAnimationWidget.staggeredDotsWave(
+        color: Colors.deepPurple,
+        size: 80,
       ),
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
